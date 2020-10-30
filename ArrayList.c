08d6eccdf13f1cae06b040ce3c869aa90f334720
Madina Saparbayeva <invisible.kichi@gmail.com>
2020-10-30 07:31:29 +0000

    createArrayList

--- ArrayList.c
+++ ArrayList.c
@@ -1,6 +1,7 @@
 /***************************************************************************************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /*-------------------------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -60,6 +61,7 @@ typedef struct arrayList
 }
 ArrayList ;
 
+
 /***************************************************************************************************************************************************/
 
 
@@ -101,12 +103,50 @@ static void setFunctionPointers ( List list )
 
 List createArrayList ( int initialCapacity )
 {
+  if (initialCapacity == LIST_NA) {
+    printf("OK2 ");
+    initialCapacity = 10;
+  }
+
+  if(initialCapacity <= 0){
+    printf("OK1 ");
+    return NULL;
+  }
+
+  ArrayList* newArrayList = malloc(sizeof(ArrayList));
+  if(newArrayList == NULL) {  return NULL;  }
+
+  //Create the internal data container
+  newArrayList->arraySize = initialCapacity;
+  newArrayList->numberOfElements = 0;
+  newArrayList->array = malloc(initialCapacity * sizeof(int));
+
+  printf("OK3 ");
+
+
+  //Allocate the List and connect it to the newArrayList
+
+  List lst = malloc(sizeof(List));
+  lst->pInternalList = newArrayList;
+  free(newArrayList);
+
+  printf("%d \n", initialCapacity);
+
+  return lst;
+
 }
 
 /*=================================================================================================================================================*/
 
 List createArrayListFrom ( List other )  /* 'other' list can be implemented using linked lists or other underlying data structures */
 {
+  ArrayList* newArrayList = malloc((int) other->size *sizeof(ArrayList));
+
+  newArrayList->numberOfElements = (int) other->size;
+  newArrayList->array = other->pInternalList;
+  newArrayList->arraySize = (int) other->size;
+
+  return (List) newArrayList;
 }
 
 /***************************************************************************************************************************************************/
@@ -115,117 +155,166 @@ List createArrayListFrom ( List other )  /* 'other' list can be implemented usin
 
 
 /***************************************************************************************************************************************************/
+/* Inserts the specified element at the specified position in the list. Shifts the element currently at that position (if any) and any subsequent
+   elements to the right (adds one to their indices). If index is LIST_NA, then appends the specified element to the end of the list.
+   Returns True if the operation is successful.
+   Returns False if the operation is unsuccessful; for example if index < 0, or index > listSize(list), or running out of memory. */
 
 ListBoolean alAdd ( List list , ListElement element , int index )
 {
+  printf("alADD");
+
+  if(list == NULL){ 
+    printf("alAdd2 ");
+    return False; 
+  }
+
+  if(index < 0 || index > listSize(list)){
+    printf("alAdd3 ");
+    return False;
+  }
+  
+  //ArrayList *data = (ArrayList*)(list->pInternalList);
+
+
+  if(index == LIST_NA){
+    printf("alAdd0 ");
+    list->add(list, element, listSize(list)+1);
+  }
+
+  list->add(list, element, index);
+
+  printf("alAdd1 ");
+
+  //list->add(list, element, index);
+
+  return True;
 }
 
 /*=================================================================================================================================================*/
 
 ListBoolean alAddAll ( List list , List other , int index )
 {
+  return False;
 }
 
 /*=================================================================================================================================================*/
 
 void alClear ( List list )
 {
+
 }
 
 /*=================================================================================================================================================*/
 
 ListBoolean alContains ( List list , ListElement element )
 {
+  return False;
 }
 
 /*=================================================================================================================================================*/
 
 ListBoolean alContainsAll ( List list , List other )
 {
+  return False;
 }
 
 /*=================================================================================================================================================*/
 
 ListBoolean alEquals ( List list , List other )
 {
+  return False;
 }
 
 /*=================================================================================================================================================*/
 
 ListElement alGet ( List list , int index )
 {
+  return 0;
 }
 
 /*=================================================================================================================================================*/
 
 int alIndexOf ( List list , ListElement element )
 {
+  return 0;
 }
 
 /*=================================================================================================================================================*/
 
 ListBoolean alIsEmpty ( List list )
 {
+  return False;
 }
 
 /*=================================================================================================================================================*/
 
 int alLastIndexOf ( List list , ListElement element )
 {
+  return False;
 }
 
 /*=================================================================================================================================================*/
 
 ListElement alRemove ( List list , int index )
 {
+  return 0;
 }
 
 /*=================================================================================================================================================*/
 
 ListBoolean alRemoveElement ( List list , ListElement element )
 {
+  return False;
 }
 
 /*=================================================================================================================================================*/
 
 ListBoolean alRemoveAll ( List list , List other )
 {
+  return False;
 }
 
 /*=================================================================================================================================================*/
 
 void alReplaceAll ( List list , ListElementUnaryOperator uOperator )
 {
+
 }
 
 /*=================================================================================================================================================*/
 
 ListBoolean alRetainAll ( List list , List other )
 {
+  return False;
 }
 
 /*=================================================================================================================================================*/
 
 ListElement alSet ( List list , int index , ListElement element )
 {
+  return 0;
 }
 
 /*=================================================================================================================================================*/
 
 int alSize ( List list )
 {
+  return False;
 }
 
 /*=================================================================================================================================================*/
 
 ListBoolean alSort ( List list , ListElementComparisonFunction compare )
 {
+  return False;
 }
 
 /*=================================================================================================================================================*/
 
 ListElement * alToArray ( List list )
 {
+  return 0;
 }
 
 /*=================================================================================================================================================*/
